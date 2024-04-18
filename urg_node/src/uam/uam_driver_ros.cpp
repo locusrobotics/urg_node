b10c021343f1241591e6c4a32fb4444d856bcb7e
Sushant <sushant.raj2018@gmail.com>
2024-04-18 13:00:19 -0400

    test

--- urg_node/src/uam/uam_driver_ros.cpp
+++ urg_node/src/uam/uam_driver_ros.cpp
@@ -177,7 +177,7 @@ void UamROS::scanWatchdogTimerCallback(const ros::TimerEvent& event)
     // Log the elapsed time since the last scan sector message and scan_timeout *2 
     ROS_INFO_STREAM("Elapsed time since last scan sector message: " << elapsed_time.toSec() << " seconds.");
     ROS_INFO_STREAM("Scan timeout: " << params_.scan_timeout << " seconds.");
-    if (elapsed_time.toSec() > params_.scan_timeout * 2)
+    if (elapsed_time > params_.scan_timeout * 2)
     {
       should_reset_lidar = true;
     }
