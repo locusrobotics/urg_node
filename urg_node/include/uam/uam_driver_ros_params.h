31e85b2f64721a4a0604f98f88643857675e1d1f
Sushant <sushant.raj2018@gmail.com>
2024-04-18 15:28:03 -0400

    cleanup

--- urg_node/include/uam/uam_driver_ros_params.h
+++ urg_node/include/uam/uam_driver_ros_params.h
@@ -133,10 +133,6 @@ public:
   ros::Duration scan_timeout { 5.0 };
 
   /**
-   * @brief If no scan sectors have been received after this many seconds, reconnect to the lidar
-   */
-
-  /**
    * @brief The topic name where LaserScan messages will be published
    */
   std::string scan_topic { "scan" };
