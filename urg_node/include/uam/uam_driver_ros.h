c1108e7e548708f1ebb9d67bc19c607ceea6caba
Owen Sullivan <osullivan@locusrobotics.com>
2026-04-20 13:12:19 -0400

    added fix for all-nan messages on boot

--- urg_node/include/uam/uam_driver_ros.h
+++ urg_node/include/uam/uam_driver_ros.h
@@ -204,6 +204,7 @@ private:
         nullptr;
 
     std::vector<std::pair<size_t, float>> ranges_in_safety_area;
+    size_t finite_ranges_count = 0;
 
     for (size_t idx = 0; idx < reply.ranges.size(); idx++)
     {
@@ -217,6 +218,10 @@ private:
       if (within_fov)
       {
         msg.ranges.push_back(range_m);
+        if (!std::isnan(range_m))
+        {
+          finite_ranges_count++;
+        }
       }
 
       if (safety_area != nullptr && !std::isnan(range_m))
@@ -227,6 +232,16 @@ private:
           ranges_in_safety_area.push_back(std::make_pair(idx, range_m));
         }
       }
+    }
+
+    if (finite_ranges_count == 0) {
+      // This driver/lidar combination will periodically boot up and publish all nan ranges for a moment.
+      // If this happens after a lidar power cycle (when the rest of the system is initilized), it breaks the
+      // laser filtering pipeline. The likleyhood of nominally receiving all nan readings in our usecase is so
+      // infinitesimally small we can just skip publishing in this case.
+      ROS_WARN_STREAM_THROTTLE(5.0, "All ranges are infinite, skipping scan publish!");
+      // We return early to avoid publishing mis-leading scans or area information.
+      return;
     }
 
     if constexpr (is_detected<detected_intensities, T>::value)
